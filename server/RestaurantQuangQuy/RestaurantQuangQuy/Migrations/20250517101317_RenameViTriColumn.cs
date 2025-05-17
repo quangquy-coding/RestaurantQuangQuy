21577c513549e3536e035f7130b6137d5bfafa8b
QuanHD0610 <quanhd@itp-link.com>
2025-05-17 17:15:01 +0700

    [DB] rename vitri on BanAN

---
+++ server/RestaurantQuangQuy/RestaurantQuangQuy/Migrations/20250517101317_RenameViTriColumn.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantQuangQuy.Migrations
+{
+    public partial class RenameViTriColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ViTri",
+                table: "BANAN",
+                newName: "viTri"
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "viTri",
+                table: "BANAN",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // ✅ Quay ngược lại tên cột ban đầu
+            migrationBuilder.RenameColumn(
+                name: "viTri",
+                table: "BANAN",
+                newName: "ViTri"
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ViTri",
+                table: "BANAN",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true
+            );
+        }
+    }
+}
